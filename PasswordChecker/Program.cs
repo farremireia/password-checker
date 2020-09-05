8ee037d3a5e02b7aa8586940650e620d34bdf1ba
Mireia Farre <67146405+farremireia@users.noreply.github.com>
2020-09-05 16:03:30 +0100

    Add files via upload

---
+++ PasswordChecker/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int minLength = 8;
+            string uppercase = "ABCDEFGHIJKLMNOPQRTUVWXYZ";
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string digits = "0123456789";
+            string specialChars = "_!@#$%^&*-+";
+
+            Console.Write("Enter a password: ");
+            string password = Console.ReadLine();
+
+            int score = 0;
+
+            if (password.Length >= minLength)
+            {
+                score++;
+            }
+            if (Tools.Contains(password, uppercase))
+            {
+                score++;
+            }
+            if (Tools.Contains(password, lowercase))
+            {
+                score++;
+            }
+            if (Tools.Contains(password, digits))
+            {
+                score++;
+            }
+            if (Tools.Contains(password, specialChars))
+            {
+                score++;
+            }
+
+            switch (score)
+            {
+                case 5:
+                case 4:
+                    Console.WriteLine("Your password is very strong.");
+                    break;
+                case 3:
+                    Console.WriteLine("Your password is strong.");
+                    break;
+                case 2:
+                    Console.WriteLine("Your password is medium.");
+                    break;
+                case 1:
+                    Console.WriteLine("Your password is weak.");
+                    break;
+                default:
+                    Console.WriteLine("Sorry, your password does not meet any of our requirements.");
+                    break;
+            }
+        }
+    }
+}
